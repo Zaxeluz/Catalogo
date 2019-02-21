094c648141b36a54ae327e404836dfaf733b1a82
Alumno <ulsa@iMacD110.local>
2019-02-20 18:10:05 -0700

    Un error loco

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+@section('titulo_pagina','Mascotas | Crear mascota')
+@section('titulo','Mascotas')
+@section('subtitulo','Crear mascota')
+@section('contenido')
+<div class="row">
+    <div class="col-md-12">
+            <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Dashboard</h3>
+            </div>
+            <div class="box-body">
+                El contenido es mi dashboard
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
